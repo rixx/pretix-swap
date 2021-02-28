27c1d7a82b2c1bee3357c1bd70e249e7ce366953
Tobias Kunze <r@rixx.de>
2021-02-28 13:23:15 +0100

    Initial commit

---
+++ pretix_swap/__init__.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext_lazy
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+__version__ = "1.0.0"
+
+
+class PluginApp(PluginConfig):
+    name = "pretix_swap"
+    verbose_name = "Swap tickets with other attendees"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Swap tickets with other attendees")
+        author = "Tobias Kunze"
+        description = gettext_lazy("Swap tickets, anonymously or with specific other attendees. Also supports canceling tickets if (and only if) another ticket is purchased.")
+        visible = True
+        version = __version__
+        category = "FEATURE"
+        compatibility = "pretix>=2.7.0"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = "pretix_swap.PluginApp"
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+from distutils.command.build import build
+
+from django.core import management
+from setuptools import find_packages, setup
+
+from pretix_swap import __version__
+
+
+try:
+    with open(
+        os.path.join(os.path.dirname(__file__), "README.rst"), encoding="utf-8"
+    ) as f:
+        long_description = f.read()
+except Exception:
+    long_description = ""
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command("compilemessages", verbosity=1)
+        build.run(self)
+
+
+cmdclass = {"build": CustomBuild}
+
+
+setup(
+    name="pretix-swap",
+    version=__version__,
+    description="Swap tickets, anonymously or with specific other attendees. Also supports canceling tickets if (and only if) another ticket is purchased.",
+    long_description=long_description,
+    url="https://github.com/rixx/pretix-swap",
+    author="Tobias Kunze",
+    author_email="r@rixx.de",
+    license="Apache",
+    install_requires=[],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[pretix.plugin]
+pretix_swap=pretix_swap:PretixPluginMeta
+""",
+)
