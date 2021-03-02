8f142564a1234ccc6fe8e2fc97e246af9d980e33
Tobias Kunze <r@rixx.de>
2021-03-02 13:13:28 +0100

    Improve secret/copyable behaviour

--- pretix_swap/static/pretix_swap/order.js
+++ pretix_swap/static/pretix_swap/order.js
@@ -1,21 +1,5 @@
-document.querySelectorAll(".copyable.secret").forEach(e => {
-    e.style.color = "transparent";
-    e.style.textShadow = "0 0 5px rgba(0,0,0,1.2)";
-    e.style.filter = "blur(5px)"
+document.querySelectorAll(".copyable").forEach(e => {
     e.style.cursor = "pointer"
-    e.style.margin = "0px 8px"
-    e.addEventListener("mouseenter", ev => {
-        const e = ev.target
-        e.style.filter = ""
-        e.style.textShadow = ""
-        e.style.color = "black"
-    })
-    e.addEventListener("mouseleave", ev => {
-        const e = ev.target
-        e.style.filter = "blur(5px)";
-        e.style.textShadow = "0 0 5px rgba(0,0,0,1.2)";
-        e.style.color = "transparent"
-    })
     e.addEventListener("click", e => {
         var $temp = $("<input>")
         $("body").append($temp)
@@ -32,5 +16,24 @@ document.querySelectorAll(".copyable.secret").forEach(e => {
           e.target.dataset["originalTitle"] = previousTitle
             $target.tooltip('hide')
         }, 600)
+    })
+})
+
+document.querySelectorAll(".secret").forEach(e => {
+    e.style.color = "transparent";
+    e.style.textShadow = "0 0 5px rgba(0,0,0,1.2)";
+    e.style.filter = "blur(5px)"
+    e.style.margin = "0px 8px"
+    e.addEventListener("mouseenter", ev => {
+        const e = ev.target
+        e.style.filter = ""
+        e.style.textShadow = ""
+        e.style.color = "black"
+    })
+    e.addEventListener("mouseleave", ev => {
+        const e = ev.target
+        e.style.filter = "blur(5px)";
+        e.style.textShadow = "0 0 5px rgba(0,0,0,1.2)";
+        e.style.color = "transparent"
     })
 })
