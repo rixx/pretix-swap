8b796e5cf8372968d3f64ad0a34e954164edd60e
Tobias Kunze <r@rixx.de>
2021-06-29 00:04:26 +0200

    Release v0.3.2

--- pretix_swap/__init__.py
+++ pretix_swap/__init__.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 
 class PluginApp(PluginConfig):
